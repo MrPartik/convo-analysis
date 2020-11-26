105ccbe5da3e4de5a97a557d9aa62d3e4c2cbf6b
Patrick Loyola <loyolapat04@gmail.com>
2020-11-26 22:24:39 +0800

    added logo CHED

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <a href="/">
+                <img src='/img/logo.png' style="height: 100px"/>
+            </a>
         </x-slot>
 
         <div x-data="{ recovery: false }">
