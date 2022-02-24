fded50d1029388c01530d9213d5dbceadd1722ab
John Patrick Loyola <loyolapat04@gmail.com>
2022-02-24 11:22:40 +0800

    UI fix

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -2,7 +2,7 @@
     $sUserRole = Auth::user()->role;
     $sRegion = Auth::user()->region;
 @endphp
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100" style="position: fixed;width: 100%;">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100" style="position: fixed;width: 100%; z-index: 999999;">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
